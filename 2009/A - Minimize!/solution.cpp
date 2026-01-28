655d45be75006544837d8c2f2f1665406dd61f6c
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-01-28 14:15:57 +0530

    Add Minimize! [A] from Codeforces

---
+++ 2009/A - Minimize!/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main(){    int t;    cin >> t;    while(t--)    {        int a,b;        cin >> a >> b;        int mina = b-a;        cout << mina << endl;    }}
