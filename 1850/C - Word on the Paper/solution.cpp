6fa4f32cf8c75cd94564b3c96ef4ce6360aa5c4b
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-01-29 23:07:20 +0530

    Add Word on the Paper [C] from Codeforces

---
+++ 1850/C - Word on the Paper/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main(){    int t;    cin >> t;    while (t--)    {        for (int i = 0; i < 8; i++)        {            string s;            cin >> s;            for (char c : s)            {                if (c != '.')                    cout << c;            }        }        cout << '\n';    }    return 0;}
