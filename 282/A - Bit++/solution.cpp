58298249549dd53a6023916ca44c9d78d8510805
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-01-29 13:43:56 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main(){    int n;    cin >> n;    int x=0;    for(int i=0;i<n;i++)    {        string s;        cin >> s;        if(s[1]=='-')        {            x = x-1;        }        else        {            x = x+1;        }    }    cout << x << endl;}
