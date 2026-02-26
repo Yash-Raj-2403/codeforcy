1d06c2b1e49eb42cfc0849131c208b621ee672b4
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-02-26 13:53:23 +0530

    Add HQ9+ [A] from Codeforces

---
+++ 133/A - HQ9+/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main(){    string s;    cin >> s;    int n = s.size();    bool found = false;    for(int i=0;i<n;i++)    {        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9' )        {            found = true;            break;        }    }    if(found){        cout << "YES" << endl;    }    else    {        cout << "NO" << endl;    }}
