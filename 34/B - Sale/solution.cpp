113ced47f93c81213725218f7a87ad3e25289a1d
Yash Raj <196000011+Yash-Raj-2403@users.noreply.github.com>
2026-03-06 22:00:02 +0530

    Add Sale [B] from Codeforces

---
+++ 34/B - Sale/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main(){    int n,k;    cin >> n >> k;    vector<int> a(n);    for(int i=0;i<n;i++)    {        cin >> a[i];    }    sort(a.begin(),a.end());    long long sum =0;    for(int i=0;i<n && k>0;i++)    {        if(a[i] < 0)        {             sum = sum + abs(a[i]);             k--;        }    }    cout << sum << endl;}
